5303ecb02593a18be562072cb55ae90b3bf19604
Shanmugha Balan <57057047+sbalan7@users.noreply.github.com>
2020-04-08 12:42:14 +0530

    Add files via upload

--- covid.py
+++ covid.py
@@ -14,6 +14,8 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
+colors = ["#00FF00", "#0000FF"]
+sns.set_palette(sns.color_palette(colors))
 
 def process_file(file_dest):
 
@@ -26,7 +28,7 @@ def process_file(file_dest):
     del contents[0]  #deletes header row
     return contents
 
-def make_ind_df(contents):
+def make_ind_df(contents, summary=False):
     
     infected=[]
     recovered=[]
@@ -72,11 +74,12 @@ def make_ind_df(contents):
     ind_dict={'Days since first infection':dates, 'Infected':infected, 'Dead':deceased, 'Recovered':recovered}
     
     df = pd.DataFrame(ind_dict)
-    df.info()
+    if summary:
+        df.info()
 
     return df
 
-def make_ita_df(contents):
+def make_ita_df(contents, summary=False):
     
     infected=[]
     recovered=[]
@@ -122,11 +125,12 @@ def make_ita_df(contents):
     ita_dict={'Days since first infection':dates, 'Infected':infected, 'Dead':deceased, 'Recovered':recovered}
     
     df = pd.DataFrame(ita_dict)
-    df.info()
+    if summary:
+        df.info()
 
     return df  
 
-def make_usa_df(contents):
+def make_usa_df(contents, summary=False):
     
     infected=[]
     recovered=[]
@@ -136,57 +140,95 @@ def make_usa_df(contents):
     
     for i in range(len(contents)):
       
-        dates.append(x)
+        dates.insert(0, x)
         x-=1
-        infected.append(int(contents[i][2]))
+        infected.insert(0, int(contents[i][2]))
         if (contents[i][14]==''):
             contents[i][14] = 0
         if (contents[i][11]==''):
             contents[i][11] = 0
-        deceased.append(int(contents[i][14]))
-        recovered.append(int(contents[i][11]))
+        deceased.insert(0, int(contents[i][14]))
+        recovered.insert(0, int(contents[i][11]))
 
     
     usa_dict={'Days since first infection':dates, 'Infected':infected, 'Dead':deceased, 'Recovered':recovered}
     
     df = pd.DataFrame(usa_dict)
-    df.info()
+    if summary:
+        df.info()
 
     return df  
+
+def make_kor_df(contents, summary=False):
+    
+    infected=[]
+    recovered=[]
+    deceased=[]
+    dates=[]
+    x = 0
+    
+    for i in range(len(contents)):
+      
+        dates.append(x)
+        x+=1
+        infected.append(int(contents[i][-3]))
+        deceased.append(int(contents[i][-1]))
+        recovered.append(int(contents[i][-2]))
+
+    
+    kor_dict={'Days since first infection':dates, 'Infected':infected, 'Dead':deceased, 'Recovered':recovered}
+    
+    df = pd.DataFrame(kor_dict)
+    
+    if summary:
+        df.info()
+
+    return df  
+
+def singlelineplot(prop, df, color, label):
+    sns.lineplot(x='Days since first infection', y=prop, data=df, color=color, label=label)
+    sns.despine()
+
+def compareproperties(prop, df1, df1_name, df2, df2_name):
+        
+    plt.figure()
+    sns.set(style='darkgrid')
+    
+    singlelineplot(prop, df1, "#00ff00", label=df1_name)
+    singlelineplot(prop, df2, "#009900", label=df2_name)
+
+    plt.title(prop)
+    plt.show()
 
 
 ind_df = make_ind_df(process_file("covid_19_india.csv"))
 ita_df = make_ita_df(process_file("covid19_italy_region.csv"))
 usa_df = make_usa_df(process_file("us_covid19_daily.csv"))
+kor_df = make_kor_df(process_file("covid19_korea.csv"))
+
+compareproperties("Infected", ind_df, "India", kor_df, "Korea")
+compareproperties("Infected", ita_df, "Italy", usa_df, "USA")
+
+
 '''
-data = {'Days since infection':ind_df['Days since first infection'], 
-        'India':ind_df['Infected'], 
-        'Italy':ita_df['Infected']
-        }
-
-df = pd.DataFrame(data)
-
-plt.plot('Days since infection', 'India', data=df, marker='', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=2)
-plt.plot('Days since infection', 'Italy', data=df, marker='', color='olive', linewidth=2)
-#plt.plot('Days since infection', 'USA', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="toto")
-plt.legend()
+plt.figure()
+x_col="Days since first infection"
+y_col="Infected"
+sns.lineplot(x=x_col, y=y_col, data=ind_df, color="green")
+sns.lineplot(x=x_col, y=y_col, data=kor_df, color="blue")
+#sns.pointplot(x=x_col, y=y_col, data=ita_df, color="red")
 
 
-sns.lineplot(x='Days since infection',
-             y='India',
-             data=df,
-             markers=['o','<','>'],
-             legend="brief"
-             )
-
-
-
-sns.lmplot(x='Days since first infection', y='Infected', data=ind_df, fit_reg=False)
-sns.despine()
-
-sns.lmplot(x='Days since first infection', y='Infected', data=ita_df, fit_reg=False)
-sns.despine()
+''
+f, ax = plt.subplots(1, 1, figsize=figsize)
+x_col='date'
+y_col = 'count'
+sns.pointplot(ax=ax,x=x_col,y=y_col,data=df_1,color='blue')
+sns.pointplot(ax=ax,x=x_col,y=y_col,data=df_2,color='green')
+sns.pointplot(ax=ax,x=x_col,y=y_col,data=df_3,color='red')
+df_1['region'] = 'A'
+df_2['region'] = 'B'
+df_3['region'] = 'C'
+df = pd.concat([df_1,df_2,df_3])
+sns.pointplot(ax=ax,x=x_col,y=y_col,data=df,hue='region')
 '''
-sns.lmplot(x='Days since first infection', y='Infected', data=usa_df, fit_reg=False)
-sns.despine()
-
